6b6a870ed8a026ac8929394623f221d9a479ac63
Ryan Braley <ryan.braley@gmail.com>
2011-03-12 15:59:13 +0800

    removed legacy move messages and stub code from before curses was implemented

--- src/game/game.py
+++ src/game/game.py
@@ -32,16 +32,6 @@ class Game(object):
         self.entities = {}
         self.direction = Vector()
 
-        # also temporary: instead of taking input,
-        # the game just pretends the character is
-        # moving in a circle
-        self.progress = 0
-        self.dir_idx = 0
-
-        # temporary; remove when curses gets added
-        #fd = sys.stdin.fileno()
-        #fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-        #fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
         self.create()
         
     def create(self):        
@@ -53,16 +43,8 @@ class Game(object):
         
     def update(self, elapsed_seconds):
         """Runs every frame"""
-        self.progress += elapsed_seconds
-        if self.progress > 1:
-            circle_map = [ (1 , 0), (0 , 1), (-1, 0), (0 , -1) ]
-            d = circle_map[self.dir_idx]
-            self.move(*d)
-            self.progress -= 1
-            self.dir_idx = (self.dir_idx + 1) % 4
-
-            self.running = True
-            self.redraw()
+        self.running = True
+        self.redraw()
 
     def add_entity(self, id, name=None):
         if id in self.entities:
@@ -101,7 +83,7 @@ class Game(object):
                 pos = entity.states['Position']
                 if entity.states.has_key('Asset'):
                     asset = entity.states['Asset']
-                    #sddelf.scr.addstr(int(pos.y),int(pos.x), '⩕⎈☸⨳⩕⩖⩕@', curses.color_pair(2))
+                    #self.scr.addstr(int(pos.y),int(pos.x), '⩕⎈☸⨳⩕⩖⩕@', curses.color_pair(2))
                     if in_bounds(pos.x,pos.y):
                         self.scr.addstr(int(pos.y),int(pos.x), asset, curses.color_pair(2))
         self.scr.refresh()
